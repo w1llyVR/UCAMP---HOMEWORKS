54b263dba2716143c272242ea3674132e6407f14
WillyValentin <89035954+WillyValentin@users.noreply.github.com>
2022-07-16 12:19:20 -0500

    Promesas

--- UCAMP -10/Callback/Ejericicio/index.js
+++ UCAMP -10/Callback/Ejericicio/index.js
@@ -13,5 +13,24 @@ const saludar = (funcion_callback, palabra) => {
     }, 4000)
 }
 
-saludar(funcion_callback, "willy")
-console.log('final')
+// saludar(funcion_callback, "willy")
+// console.log('final')
+
+
+const saludar2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+
+        resolve('Esto es un resolve') //Funciona como retun es decir acaba la funcion
+        reject('Esto es un reject') //Como "error"
+
+    }, 4000)
+});
+
+
+
+console.log('dfdf', 'dfdfd', 12, "")
+
+console.log(saludar2);
+saludar2.then((response) => {
+    console.log(response)
+});
