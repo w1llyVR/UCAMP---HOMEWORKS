65b0586971a323637a2a493fcda0bb954357fbb8
WillyValentin <wvaleric@gmail.com>
2022-07-27 16:10:09 -0500

    Uso de map para renderizar

--- UCAMP - 13/Ejemplo/mi-primer-proyecto/src/App.js
+++ UCAMP - 13/Ejemplo/mi-primer-proyecto/src/App.js
@@ -3,6 +3,20 @@ import './App.css';
 import Component from './components/Component';
 import Hello from './components/Hello';
 
+const arr = [{
+        name: "Willy",
+        age: 19
+    },
+    {
+        name: "Miguel",
+        age: 20
+    },
+    {
+        name: "Angie",
+        age: 22
+    }
+]
+
 function App() {
     return ( <
         div className = "App" >
@@ -10,7 +24,19 @@ function App() {
         Component / >
         <
         Hello name = "Willy" / >
+
         <
+        h1 > USO DE MAP CON REACT < /h1> {
+            arr.map((persona, i) => {
+                return ( <
+                    div key = { i } >
+                    <
+                    h1 > { persona.name } < /h1> <
+                    p > { persona.age } < /p> <
+                    /div>
+                )
+            })
+        } <
         /div>
     );
 }
