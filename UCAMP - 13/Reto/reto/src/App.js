d004358f5c52d4f65f4e7d225c1f4a6b8fd2b71a
WillyValentin <wvaleric@gmail.com>
2022-07-30 15:54:51 -0500

    .

--- UCAMP - 13/Reto/reto/src/App.js
+++ UCAMP - 13/Reto/reto/src/App.js
@@ -40,3 +40,49 @@ function App() {
 }
 
 export default App;
+
+
+/*
+import logo from './logo.svg';
+import './App.css';
+import SetText from './Components/SetText';
+
+var pacientes = [
+  {
+    name: "Roberto",
+    age: 25
+  },
+  {
+    name: "Martin",
+    age: 20
+  },
+  {
+    name: "Carlos",
+    age: 15
+  },
+  {
+    name: "Benito",
+    age: 80
+  },
+  {
+    name: "Luis",
+    age: 50
+  },
+  {
+    name: "Pepe",
+    age: 60
+  }
+]
+
+
+function App() {
+  return (
+    <div className="App">
+      <SetText arreglo = {pacientes}/>
+    </div>
+  );
+}
+
+export default App;
+
+*/
